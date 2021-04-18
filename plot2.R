03a837b4db9c4f0064d7fe2ecabb2b77362e08a3
Harsh Yadav <71450518+harshyadav26@users.noreply.github.com>
2021-04-18 20:24:21 +0530

    Add files via upload

--- plot2.R
+++ plot2.R
@@ -1,26 +1,22 @@
 library("data.table")
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
 
-setwd("~/Desktop/datasciencecoursera/4_Exploratory_Data_Analysis/project/data")
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
 
-#Reads in data from file then subsets data for specified dates
-powerDT <- data.table::fread(input = "household_power_consumption.txt"
-                             , na.strings="?"
-)
+NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
+totalNEI <- NEI[fips=='24510', lapply(.SD, sum, na.rm = TRUE)
+                , .SDcols = c("Emissions")
+                , by = year]
 
-# Prevents Scientific Notation
-powerDT[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+png(filename='plot2.png')
 
-# Making a POSIXct date capable of being filtered and graphed by time of day
-powerDT[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
-
-# Filter Dates for 2007-02-01 and 2007-02-02
-powerDT <- powerDT[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
-
-png("plot2.png", width=480, height=480)
-
-## Plot 2
-plot(x = powerDT[, dateTime]
-     , y = powerDT[, Global_active_power]
-     , type="l", xlab="", ylab="Global Active Power (kilowatts)")
+barplot(totalNEI[, Emissions]
+        , names = totalNEI[, year]
+        , xlab = "Years", ylab = "Emissions"
+        , main = "Emissions over the Years")
 
 dev.off()
